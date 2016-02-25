95a58b8fae23399b11234782c07565558612b19f
Bikram <bikram@Bikrams-MacBook-Pro.local>
2016-02-24 19:01:12 -0800

    first push

---
+++ app01.js
@@ -0,0 +1,12 @@
+// Geekwise HS1 - Day 1 Notes
+// Bikram Sekhon
+// 2/23/2016
+
+"use strict";
+
+var a = 12;
+var b = 0x1E;
+var c = "Answer Equals: ";
+var sum = a + b;
+var answer = c + sum;
+console.log(answer)
---
+++ day1.js
@@ -0,0 +1,15 @@
+// Geekwise HS1 - Day 1 Notes
+// Bikram Sekhon
+// 2/22/2016
+
+"use strict";
+
+// alert("Hell world");
+
+
+
+
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c);
